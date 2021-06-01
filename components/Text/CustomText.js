7a07fae0beda145875a9d619668b55b249fb88a4
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-01 15:04:28 +0200

    Added comment to the custom text

--- components/Text/CustomText.js
+++ components/Text/CustomText.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
 
+// HOC to render customized fonts
 export default function CustomText(props) {
   return <Text style={{ ...props.style, ...styles.text  }} >{props.children}</Text>
 }
