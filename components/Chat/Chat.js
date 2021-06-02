198c4116a78a7f7e8cb4a9f94bc89321e60b8267
Julio Lopez <julioj.lopeza@gmail.com>
2021-06-02 13:47:23 +0200

    Added comments to the chat component

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -5,11 +5,19 @@ import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 // Component renders the chat view
 export default function Chat(props) {
   const [ messages, setMessages ] = useState([])
+  const name = props.route.params.name
 
+  // Used useeffect to load some mock messages
   useEffect(() => {
     setMessages([
       {
         _id: 1,
+        text: `${name} has joined to the chat`,
+        createdAt: new Date(),
+        system: true,
+      },
+      {
+        _id: 2,
         text: 'Hello developer',
         createdAt: new Date(),
         user: {
@@ -18,15 +26,10 @@ export default function Chat(props) {
           avatar: 'https://placeimg.com/140/140/any',
         },
       },
-      {
-        _id: 2,
-        text: 'This is a system message',
-        createdAt: new Date(),
-        system: true,
-      },
     ])
   }, [])
 
+  // Function to send a new message
   const onSend = messages => {
     setMessages(previousState => GiftedChat.append(previousState, messages))
   }
@@ -34,13 +37,14 @@ export default function Chat(props) {
   return (
     <View style={styles.container}>
       <GiftedChat
-        renderBubble={renderBubble}
+        renderBubble={renderBubble} // Attribute to customize the chat bubble
         messages={messages}
         onSend={messages => onSend(messages)}
         user={{
           _id: 1,
         }}
       />
+      {/* Fixed Keyboard hiding the input for android devices */}
       { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
     </View>
   )
